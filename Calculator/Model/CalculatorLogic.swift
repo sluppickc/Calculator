c959bc91494a8994b73f892b6027877dfc728dff
Chris Sluppick <sluppickc@gmail.com>
2019-06-28 15:46:13 -0500

    Finish operation logic and clean up code

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,22 +10,50 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
+    private var number: Double?
     
-    init(number: Double) {
+    private var intermediateCalculation:(n1: Double, operator: String)?
+    
+    mutating func setNumber(to number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "AC" {
-            return 0
-        } else if symbol == "%" {
-            return number / 100
-        }
+    mutating func calculate(symbol: String) -> Double? {
         
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n / 100
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                self.intermediateCalculation = (n1: n, operator: symbol)
+            }
+        }
         return nil
     }
     
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.operator {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed does not match any of the cases")
+            }
+        }
+        return nil
+    }
 }
